72e42e95c7f052cd2a9bd142270099af230dc0cd
jeed.xie@gmail.com <jeed.xie@gmail.com>
2025-08-07 10:11:15 +0000

    a

--- models/curated/new_york_bike_trip.sql
+++ models/curated/new_york_bike_trip.sql
@@ -1,25 +1,25 @@
 SELECT 
-
-tripduration_inseconds ,
-  starttime ,
-  stoptime ,
-  start_station_id ,
-  start_station_name ,
-  start_station_lat ,
-  start_station_lon ,
-  end_station_id ,
-  end_station_name ,
-  end_station_lat ,
-  end_station_lon ,  
-  bikeid ,
-  un_known  ,
-  usertype ,
-  birth_year ,
-  gender ,
-  6371 * ACOS(
-    COS(RADIANS(start_station_lat)) * COS(RADIANS(end_station_lat)) *
-    COS(RADIANS(end_station_lon) - RADIANS(start_station_lon)) +
-    SIN(RADIANS(start_station_lat)) * SIN(RADIANS(end_station_lat))
-  ) AS trip_distance_km,
-  update_time  CURRENT_TIMESTAMP
-from {{ source("bronze", "new_york_bike_trip") }}
+  tripduration_inseconds,
+    starttime,
+    stoptime,
+    start_station_id,
+    start_station_name,
+    start_station_lat,
+    start_station_lon,
+    end_station_id,
+    end_station_name,
+    end_station_lat,
+    end_station_lon,
+    bikeid,
+    un_known,
+    usertype,
+    birth_year,
+    gender,
+    6371 * acos(
+        cos(radians(start_station_lat))
+        * cos(radians(end_station_lat))
+        * cos(radians(end_station_lon) - radians(start_station_lon))
+        + sin(radians(start_station_lat)) * sin(radians(end_station_lat))
+    ) as trip_distance_km,
+    current_timestamp as update_time 
+from bronze.new_york_bike_trip
